08d7c90d208b4a1fc052d07bf5f82e3171f56e21
Leofric Govan <leogovan@Leos-MacBook-Pro-2.local>
2017-02-15 21:40:58 +0000

    2nd version, 2 cats and names

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-$( "#kittyClick" ).click(function() {
+$( "#kittyClick1, #kittyClick2" ).click(function() {
 	var counter = $('#TextBox').val();
             counter++ ;
             $('#TextBox').val(counter);
 
 });
+
+var catName1 = "Dave";
+var catName2 = "Steve";
+
+$("#catNameElem1").append(catName1);
+$("#catNameElem2").append(catName2);
