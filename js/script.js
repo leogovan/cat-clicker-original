8c15b61aadf6978aba9a7d140f056155931a52cc
Leofric Govan <leogovan@Leos-MacBook-Pro-2.local>
2017-03-20 20:26:53 +0000

    Got it working but needs improving to add cat at start

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-//*
-
-// ------------- PREMIUM ---------------
-
 // ------------- MODEL ---------------
 
 window.addEventListener('load', 
@@ -21,12 +17,18 @@ var model = {
 	catNamesList: ["Dave", "Steve", "Tim", "Eustace", "Alan"], 
 	// Array of cat photos
 	catPhotosList: ["img/kitty-1.jpg", "img/kitty-2.jpg", "img/kitty-3.jpg", "img/kitty-4.jpg", "img/kitty-5.jpg"],
-
+	// Place to store the current displayed cat
+	currentCat: null
 };
 
 // ------------- OCTOPUS ---------------
 
 var octopus = {
+	/*
+	init: function(){
+		makeKitties(5);
+		model.currentCat = this.kittyList[0];
+	},*/
 	// Place to store list of kitty objects
 	kittyList: [],
 	// Cat object constructor
@@ -64,16 +66,13 @@ var view = {
 		});
 	},
 
-	// Place to store the current displayed cat
-	currentCat: octopus.kittyList[0],
-
 	// Click cat name to update name, photo and score
 	// Has already collected the clicked element id
 	listClickFunc: function(clicked_id) {
 		// loop through kittyList
 		octopus.kittyList.forEach(function(i){
 			if (clicked_id === i.name){
-				currentCat = i;
+				model.currentCat = i;
 				document.querySelector("#name").innerHTML = i.name;
 				document.querySelector("input").setAttribute("src", i.photo);
 				document.querySelector("#score").innerHTML = i.clickScore;
@@ -89,110 +88,10 @@ var view = {
 		// register the click event against the collected image
 		kittyImage.onclick = function (){
 			// increase the counter value on each click
-			this.currentCat.clickScore++;
+			model.currentCat.clickScore++;
 			// write the value to the page
-			document.querySelector("#score").innerHTML = currentCat.clickScore;
+			document.querySelector("#score").innerHTML = model.currentCat.clickScore;
 			// Update a given cat's clickScore
 		};
 	}
 };
-
-//*/
-
-
-// ------------- ORIGINAL ---------------
-
-
-/*
-var catNamesList = [
-	"Dave",
-	"Steve",
-	"Tim",
-	"Eustace",
-	"Alan"
-
-];
-
-var catPhotosList = [
-	"img/kitty-1.jpg",
-	"img/kitty-2.jpg",
-	"img/kitty-3.jpg",
-	"img/kitty-4.jpg",
-	"img/kitty-5.jpg"
-];
-
-// Cat object constructor
-
-var kittyObject = function (kittyNumber) {
-	var kittyIndex = kittyNumber - 1;
-	this.index = kittyIndex;
-	this.position = kittyIndex + 1;
-	this.name = catNamesList[kittyIndex];
-	this.photo = catPhotosList[kittyIndex];
-	this.clickScore = 0;
-};
-
-// Instantiate all the cats
-
-var kittyOne = new kittyObject(1);
-var kittyTwo = new kittyObject(2);
-var kittyThree = new kittyObject(3);
-var kittyFour = new kittyObject(4);
-var kittyFive = new kittyObject(5);
-
-
-// Place to store list of kitty objects
-var kittyList = [kittyOne, kittyTwo, kittyThree, kittyFour, kittyFive];
-
-
-// Render cat names within unordered list
-function initKittyList() {
-	kittyList.forEach(function(i){
-		var li = document.createElement("li");
-		li.innerHTML = i.name;
-		document.querySelector("ul").appendChild(li);
-		// sets id attribute value - used for comparison later
-		li.setAttribute("id", i.name);
-		// sets onclick attribute, also passes through the element's id attribute value
-		li.setAttribute("onclick", "listClickFunc(this.id)");
-	});
-};
-
-// Execute the list
-initKittyList();
-
-
-// Place to store the current displayed cat
-var currentCat = kittyList[0];
-
-// Click cat name to update name, photo and score
-// Has already collected the clicked element id
-function listClickFunc(clicked_id) {
-	// loop through kittyList
-	kittyList.forEach(function(i){
-		if (clicked_id === i.name){
-			currentCat = i;
-			document.querySelector("#name").innerHTML = i.name;
-			document.querySelector("input").setAttribute("src", i.photo);
-			document.querySelector("#score").innerHTML = i.clickScore;
-		}
-	});	
-};
-
-
-
-// Increases the score for clicking a cat
-// Also tracks score for individual cats
-function increaseKittyCounter(){
-	// grabbing image and set initial counter value
-	var kittyImage = document.querySelector("input");
-	// register the click event against the collected image
-	kittyImage.onclick = function (){
-		// increase the counter value on each click
-		currentCat.clickScore++;
-		// write the value to the page
-		document.querySelector("#score").innerHTML = currentCat.clickScore;
-		// Update a given cat's clickScore
-	};
-};
-*/
